8cddd87171402775e3015cd2534af5bf86322c9a
Michael <michael.s.seitz@gmail.com>
2021-05-01 01:33:36 +0200

    Merge branch 'main' of https://github.com/PKuebler/robowars2021 into main
    # Conflicts:
    #	main.py

---
+++ gameLogic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pygame
+
+def handleEvents(event, playerTurn, moveMode, playerOne, playerOneRobot, playerTwoRobot):
+    #Mausklick auswerten
+    if pygame.MOUSEBUTTONUP:
+        pass
+    #Tastatur auswerten
+    elif pygame.KEYUP:
+        if moveMode:
+
+    return playerTurn, modeMode
---
+++ objects.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+class Object:
+
+    def __init__(self, name, obtype, health, x, y, player=0, steps=0):
+        self.name = name
+        self.obtype = obtype
+        self.health = health
+        self.x = x
+        self.y = y
+        self.player = player
+        self.steps = steps
+        self.targetX = None
+        self.targetY = None
+        self.order = None
+
+    def onDeath(self):
+        pass
+
+    def shoot(self, targetX, targetY, terrainMap, objectMap):
+        pass
+
+    def melee(self, targetX, targetY, terrainMap, objectMap):
+        pass
+
+    def move(self, targetX, targetY, terrainMap, objectMap):
+        pass
+
+    def printInfos(self):
+        print(self.name + "," + str(self.health))
