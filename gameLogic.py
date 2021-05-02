2ef6ae205a3a70df2c762eb560068ece37c75655
LRod1 <lars.stintzing@gmx.net>
2021-05-02 11:23:11 +0200

    .wav

--- gameLogic.py
+++ gameLogic.py
@@ -122,7 +122,7 @@ def executeOrders(orders, terrainMap, objectMap, playerOneRobot, playerTwoRobot,
         #bewegung
         if order["ordertype"] == "move":
             print("moving")
-            pymixer.play(pygame.mixer.Sound("204431__jaraxe__robot-walk.ogg"))
+            pymixer.play(pygame.mixer.Sound("204431__jaraxe__robot-walk.wav"))
             #pygame.mixer.play(pygame.mixer.Sound("204431__jaraxe__robot-walk.ogg"))
             #bewegung ausführen über objekt
             playerRobot.executeMove(terrainMap, objectMap, order["x"], order["y"])
@@ -136,13 +136,13 @@ def executeOrders(orders, terrainMap, objectMap, playerOneRobot, playerTwoRobot,
         #action_1
         if order["ordertype"] == "action_1":
             print("action_1")
-            pymixer.play(pygame.mixer.Sound("517939__slopemstr__laser-artillery-sound-effect.ogg"))
+            pymixer.play(pygame.mixer.Sound("517939__slopemstr__laser-artillery-sound-effect.wav"))
             playerRobot.action_1(order["x"], order["y"], terrainMap, objectMap)
         #action2
         elif order["ordertype"] == "action_2":
             print("action_2")
             playerRobot.action_2(order["x"], order["y"], terrainMap, objectMap)
-            pymixer.play(pygame.mixer.Sound("399303__deleted-user-5405837__explosion-012.ogg"))
+            pymixer.play(pygame.mixer.Sound("399303__deleted-user-5405837__explosion-012.wav"))
     visualize(terrainMap, objectMap)
     playerOneRobot.initNewRound()
     playerTwoRobot.initNewRound()
